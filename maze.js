0cb7fdd40e4d99768a4faab3ac3d0f588a4cd211
Elchadders <davychd@yahoo.com>
2016-10-14 12:45:11 -0700

    This is exercise 1

---
+++ maze.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+	
+   document.getElementById('boundary1').onmouseover=start;
+}     
+    
+function start() {
+	
+	document.getElementById('boundary1').className="boundary youlose";
+}
